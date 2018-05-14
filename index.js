7111dcbfe0dbf5d42b270f3c341804b0e58aae83
Rafael Rocha <rocha.rafaelsilva@gmail.com>
2018-05-13 22:06:52 -0300

    fix: JSDoc tags

--- index.js
+++ index.js
@@ -9,8 +9,8 @@
 /**
  * Swap the endianness of units of information in a byte array.
  * The original array is modified in place.
- * @param {!Array<number|string>|Uint8Array} bytes The bytes.
- * @param {!number} offset The number of bytes of each unit of information.
+ * @param {!Array<number|string>|!Uint8Array} bytes The bytes.
+ * @param {number} offset The number of bytes of each unit of information.
  */
 function endianness(bytes, offset) {
     let len = bytes.length;
@@ -24,9 +24,9 @@ function endianness(bytes, offset) {
 /**
  * Swap the endianness of a unit of information in a byte array.
  * The original array is modified in place.
- * @param {!Array<number|string>|Uint8Array} bytes The bytes.
- * @param {!number} offset The number of bytes of the unit of information.
- * @param {!number} index The start index of the unit of information.
+ * @param {!Array<number|string>|!Uint8Array} bytes The bytes.
+ * @param {number} offset The number of bytes of the unit of information.
+ * @param {number} index The start index of the unit of information.
  */
 function swap(bytes, offset, index) {
     let x = 0;
